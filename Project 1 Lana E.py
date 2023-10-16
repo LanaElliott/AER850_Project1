13921f974f73fdd8492592661a76014ddf1d7d4f
LanaElliott <134975988+LanaElliott@users.noreply.github.com>
2023-10-15 21:09:31 -0400

    Create Project 1 Lana E.py
    Starting Point

---
+++ Project 1 Lana E.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 15 18:50:16 2023
+
+@author: Owner
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data\\Project 1 Data.csv")
+
